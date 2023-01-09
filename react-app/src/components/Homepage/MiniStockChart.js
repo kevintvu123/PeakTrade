6229e5bef8fe3865c4b957be0be1bac2d7620eb9
Kevin <kevintvu123@gmail.com>
2023-01-08 19:01:47 -0500

    signup css completed

--- react-app/src/components/Homepage/MiniStockChart.js
+++ react-app/src/components/Homepage/MiniStockChart.js
@@ -10,34 +10,37 @@ export default function MiniStockChart({ stockTicker }) {
     const YHapiKey = process.env.REACT_APP_YH_API_KEY
     const yhUrl = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart?interval=5m&symbol=${stockTicker}&range=1d&region=US&includePrePost=false&useYfid=true&includeAdjustedClose=true&events=capitalGain%2Cdiv%2Csplit`
 
-    useEffect(async () => {
-        await fetch(yhUrl, {
-            "method": "GET",
-            "headers": {
-                'X-RapidAPI-Key': YHapiKey,
-                'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
-            }
-        })
-            .then(res => res.json())
-            .then(res => {
-                const stockData = (res['chart']['result'][0])
-                const closePrices = stockData['indicators']['quote'][0]['close']
-
-                //Normalizing data for Rechart
-                const data = closePrices.map((price, index) => ({
-                    index: index,
-                    value: price
-                }))
-
-                setStockChartData(data)
+    useEffect(() => {
+        async function fetchData() {
+            await fetch(yhUrl, {
+                "method": "GET",
+                "headers": {
+                    'X-RapidAPI-Key': YHapiKey,
+                    'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
+                }
             })
-            .then(() => {
-                setFetchLoaded(true)
+                .then(res => res.json())
+                .then(res => {
+                    const stockData = (res['chart']['result'][0])
+                    const closePrices = stockData['indicators']['quote'][0]['close']
 
-            })
-            .catch(err => {
-                console.error('errors:' + err)
-            })
+                    //Normalizing data for Rechart
+                    const data = closePrices.map((price, index) => ({
+                        index: index,
+                        value: price
+                    }))
+
+                    setStockChartData(data)
+                })
+                .then(() => {
+                    setFetchLoaded(true)
+
+                })
+                .catch(err => {
+                    console.error('errors:' + err)
+                })
+        }
+        fetchData()
     }, [YHapiKey, yhUrl])
 
     if (!stockChartData) return null
