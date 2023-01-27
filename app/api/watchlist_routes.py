85377424919f4d72e0188dfc83dd350bdd0ffaae
Kevin <kevintvu123@gmail.com>
2023-01-26 19:11:05 -0500

    added create functionality for watchlist route

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from flask_login import login_required, current_user
 from app.models import db, Watchlist, Watchlist_Stock
+from app.forms import WatchlistForm
 from .auth_routes import validation_errors_to_error_messages
 
 watchlist_routes = Blueprint("watchlists", __name__)
@@ -12,5 +13,18 @@ def create_watchlist():
     """
     Create new watchlist
     """
+    data = request.get_json()
+    data["csrf_token"] = request.cookies["csrf_token"]
+    user_id = current_user.id
 
-    return "SUCCESS"
+    form = WatchlistForm(**data)
+
+    if form.validate_on_submit():
+        create_watchlist = Watchlist(name=data["name"], user_id=user_id)
+
+        db.session.add(create_watchlist)
+        db.session.commit()
+
+        return create_watchlist.to_dict()
+
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -3,3 +3,4 @@ from .signup_form import SignUpForm
 from .transaction_form import TransactionForm
 from .stock_form import StockForm
 from .group_form import GroupForm
+from .watchlist_form import WatchlistForm
