5b7fa006b9a230a3ff28d1982fce12af2a80f9e8
Kevin Vu <kevintvu123@gmail.com>
2023-04-04 08:55:06 -0400

    add unit Pytest for transaction and stock db model

--- app/test_config.py
+++ app/test_config.py
@@ -1,4 +1,6 @@
 class TestConfig:
     TESTING = True
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:' creates a temporary in-memory SQLite database
+    # destroyed as soon as the tests finish running. Data stored will not persist!!
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-from app.models import db, User
+from app.models import db, User, Stock, Transaction
 from app import create_test_app
 #run "pipenv run pytest" in terminal even in pipenv shell to run the test
 
@@ -29,3 +29,77 @@ def test_user_creation(test_app):
 
         assert user.id is not None
         assert user in db.session
+        assert user.buying_power == 10000.00
+        assert user.password != 'password'
+        assert user.check_password('password')
+
+def test_stock_creation(test_app):
+    with test_app.app_context():
+        user = User(
+            email="testuser@example.com",
+            first_name="Test",
+            last_name="User",
+            password="password"
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        stock = Stock(
+            user_id=user.id,
+            name="Test Stock",
+            ticker="TEST",
+            amount=10,
+            avg_stock_value=20.5
+        )
+
+        db.session.add(stock)
+        db.session.commit()
+
+        # Assert that the stock has been added to the database with the correct values
+        assert stock.id is not None
+        assert user.id is not None
+        assert user in db.session
+        assert stock in db.session
+        assert stock.user_id == user.id
+        assert stock.name == "Test Stock"
+        assert stock.ticker == "TEST"
+        assert stock.amount == 10
+        assert stock.avg_stock_value == 20.5
+
+        # Assert that the to_dict method returns the correct values
+        assert stock.to_dict() == {
+            "id": stock.id,
+            "name": "Test Stock",
+            "ticker": "TEST",
+            "amount": 10,
+            "avgStockValue": 20.5
+        }
+
+def test_transaction_creation(test_app):
+    with test_app.app_context():
+        user = User(
+            email="testuser@example.com",
+            first_name="Test",
+            last_name="User",
+            password="password"
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        transaction = Transaction(
+            user_id=user.id,
+            name="Test Stock",
+            ticker="TEST",
+            price= 20.5,
+            quantity= 10,
+            order_type= 'buy'
+        )
+
+        db.session.add(transaction)
+        db.session.commit()
+
+        assert user.id is not None
+        assert transaction.id is not None
+        assert user in db.session
+        assert transaction in db.session
+        assert transaction.user_id == user.id
